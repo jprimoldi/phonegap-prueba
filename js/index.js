eb50e4a23c2eadb2a949e3401de81aaea079de8b
JRimoldi <JRimoldi>
2017-05-05 15:35:41 -0300

    Mas cambios

--- js/index.js
+++ js/index.js
@@ -36,33 +36,53 @@ var _config = {
 (function (_config, window) {
 
     angular.module('donweb', ['ngCookies'])
+        .factory('usuarioService', usuarioService)
+        .factory('notificacionesService', notificacionesService)
         .controller('AppController', AppController);
 
-    function AppController ($http, $sce, $cookies) {
+    function AppController ($http, $sce, $cookies, usuarioService, notificacionesService) {
 
         var self = this;
 
+        self.cargando = true;
+        self.usuario = usuarioService;
+        self.notificaciones = notificacionesService;
         self.errorLogin;
         self.parcial = {cargando:false};
 
         // Métodos
+        self.init = init;
         self.onDeviceReady = onDeviceReady;
         self.setupPush = setupPush;
         self.enviarRegID = enviarRegID;
         self.login = login;
+        self.logout = logout;
     
         // Bind any events that are required on startup. Common events are:
         // 'load', 'deviceready', 'offline', and 'online'.
-        window.document.addEventListener('deviceready', self.onDeviceReady, false);
+        window.document.addEventListener('deviceready', self.init, false);
 
         ////////////
+
+        function init () {
+
+            if ( ! localStorage.getItem('notificacionesDW') || localStorage.getItem('notificacionesDW').indexOf('items') < 0) {
+
+                localStorage.setItem('notificacionesDW', '{"items":[]}');
+            }
+
+            angular.merge(notificacionesService.items, JSON.parse(localStorage.getItem('notificacionesDW')).items);
+
+            obtenerResumenDeServicios().then(function () {
+                self.onDeviceReady();
+            });
+        }
 
         function onDeviceReady (argument) {
             self.setupPush();
         }
 
         function setupPush () {
-
             var push = PushNotification.init(_config);
 
             push.on('registration', function(data) {
@@ -72,19 +92,6 @@ var _config = {
                 if (oldRegId !== data.registrationId) {
                     localStorage.setItem('registrationId', data.registrationId);
                 }
-
-                var oldRegId = $cookies.get('registrationId');
-                
-                if (oldRegId !== data.registrationId) {
-                    $cookies.put('registrationId', data.registrationId);
-                }
-
-                navigator.notification.alert(
-                    $cookies.get('registrationId'),
-                    null,
-                    'Chequeando cookie!',
-                    'Ok'
-                );
 
                 navigator.notification.alert(
                     localStorage.getItem('registrationId'),
@@ -136,9 +143,9 @@ var _config = {
 
         function login (form) {
 
-            console.log(form);
-
             if (form && form.usuario && form.password) {
+
+                self.cargando = true;
 
                 var url = 'https://donweb.com/ajax-login.php';
                 // OAuthProvider: OAuthProvider,
@@ -154,18 +161,133 @@ var _config = {
                 })
                 .then(function (response) {
 
-                    if (response && response.jsonMC && response.jsonMC.resultado) {
+                    if (response.data && response.data.root.site && response.data.root.site.login) {
 
-                        console.log(response.jsonMC.respuesta);
+                        console.log(response.data.root.site.login);
+
+                        angular.merge(usuarioService, response.data.root.site.login);
+                        usuarioService.logueado = true;
+
                     } else {
 
-                        self.errorLogin = response.jsonMC.error;
+                        self.errorLogin = response.data.root.site.error;
+                        usuarioService.logueado = false;
                     }
+
+                    self.cargando = false;
                 });                
             }
+        }
+
+        function logout () {
+
+            self.cargando = true;
+
+            var url = 'https://donweb.com/ajax-login.php?accion=logout';
+            // OAuthProvider: OAuthProvider,
+            // OAuthState: OAuthState
+            $http.jsonp($sce.trustAsResourceUrl(url), {
+                jsonpCallbackParam: 'jsoncallback',
+                // params: {
+                //     account:'old',
+                //     usernameLogin: form.usuario,
+                //     passLogin: form.password,
+                //     accion: 'login'
+                // }
+            })
+            .then(function (response) {
+
+                if (response && response.data.root.site && response.data.root.site.login) {
+
+                    console.log(response.data.root.site.login);
+
+                    angular.merge(usuarioService, response.data.root.site.login);
+                    usuarioService.logueado = false;
+
+                } else {
+
+                    self.errorLogin = response.data.root.site.error;
+                }
+
+                self.cargando = false;
+            });                
+        }
+
+
+        function obtenerResumenDeServicios () {
+
+            self.cargando = true;
+
+            var url = 'https://micuenta.donweb.com/ajax-json/clientes/perfil/resumenDeServicios';
+
+            return $http.jsonp($sce.trustAsResourceUrl(url), {jsonpCallbackParam: 'jsoncallback'})
+                .then(function (response) {
+                    
+                    if (chequearLogin(response)) {
+
+                        if (response && response.data.jsonMC && response.data.jsonMC.resultado) {
+
+                            console.log(response.data.jsonMC.respuesta);
+                        } else {
+
+                            self.errorLogin = response.data.jsonMC.error;
+                        }
+
+                        self.cargando = false;
+                    }
+                });
 
         }
 
+        function chequearLogin (response) {
+
+            if ( ! response.data.jsonMC.session) {
+
+                usuarioService.logueado = false;
+                self.cargando = false;
+                console.log('usuario no logueado');
+                return false;
+
+            } else {
+                usuarioService.logueado = true;
+                console.log('usuario logueado');
+                return true;
+            }
+        }
+    }
+
+    function usuarioService () {
+
+        var self = this;
+
+        self.usuario = null;
+        self.logueado = false;
+
+        return self;
+    }
+
+    function notificacionesService () {
+
+        var self = this;
+
+        self.items = [];
+        self.agregar = agregar;
+
+        return self;
+
+        ////////////
+
+        function agregar (item) {
+
+            self.ls = JSON.parse(localStorage.getItem('notificacionesDW'));
+
+            self.ls.items.push(item);
+
+            angular.merge(self.items, self.ls.items);
+
+            localStorage.setItem('notificacionesDW', JSON.stringify(self.ls));
+        }
     }
 
 })(_config, window);
+
