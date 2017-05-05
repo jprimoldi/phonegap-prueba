047c3ead70b99e48a780e5e34e24c3646be8654d
JRimoldi <JRimoldi>
2017-05-05 09:41:14 -0300

    Mas cambios

--- js/index.js
+++ js/index.js
@@ -16,77 +16,156 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log('Received Device Ready Event');
-        console.log('calling setup push');
-        app.setupPush();
-    },
-    setupPush: function() {
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "442931165475"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
 
-        var parentElement = document.getElementById('registration');
-        var divMensaje = parentElement.querySelector('.mensaje');
+var _config = {
+    "android": {
+        "senderID": "442931165475",
+        "sound": true,
+        "vibration": true
+    },
+    "browser": {},
+    "ios": {
+        "sound": true,
+        "vibration": true,
+        "badge": true
+    },
+    "windows": {}
+};
 
-        push.on('registration', function(data) {
-            console.log('registration event: ' + data.registrationId);
 
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
+(function (_config, window) {
+
+    angular.module('donweb', ['ngCookies'])
+        .controller('AppController', AppController);
+
+    function AppController ($http, $sce, $cookies) {
+
+        var self = this;
+
+        self.errorLogin;
+        self.parcial = {cargando:false};
+
+        // Métodos
+        self.onDeviceReady = onDeviceReady;
+        self.setupPush = setupPush;
+        self.enviarRegID = enviarRegID;
+        self.login = login;
+    
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        window.document.addEventListener('deviceready', self.onDeviceReady, false);
+
+        ////////////
+
+        function onDeviceReady (argument) {
+            self.setupPush();
+        }
+
+        function setupPush () {
+
+            var push = PushNotification.init(_config);
+
+            push.on('registration', function(data) {
+
+                var oldRegId = localStorage.getItem('registrationId');
+                
+                if (oldRegId !== data.registrationId) {
+                    localStorage.setItem('registrationId', data.registrationId);
+                }
+
+                var oldRegId = $cookies.get('registrationId');
+                
+                if (oldRegId !== data.registrationId) {
+                    $cookies.put('registrationId', data.registrationId);
+                }
+
+                navigator.notification.alert(
+                    $cookies.get('registrationId'),
+                    null,
+                    'Chequeando cookie!',
+                    'Ok'
+                );
+
+                navigator.notification.alert(
+                    localStorage.getItem('registrationId'),
+                    null,
+                    'Chequeando localStorage!',
+                    'Ok'
+                );
+
+                divMensaje.innerHTML = data.registrationId;
+            });
+
+            push.on('error', function(e) {
+                console.log("push error = " + e.message);
+            });
+
+            push.on('notification', function(data) {
+
+                navigator.notification.alert(
+                    data.message,         // message
+                    null,                 // callback
+                    data.title,           // title
+                    'Ok'                  // buttonName
+                );
+           });
+        }
+
+        function enviarRegID () {
+
+            self.parcial.cargando = true;
+
+            var url = 'https://micuenta.donweb.com/ajax-json/clientes/dispositivos/registrarDispositivo';
+
+            $http.jsonp($sce.trustAsResourceUrl(url), {jsonpCallbackParam: 'jsoncallback'})
+                .then(function (response) {
+                    console.log('desde el callback', response.data.jsonMC);
+
+                    if (response && response.data.jsonMC && response.data.jsonMC.resultado) {
+
+                        console.log(response.data.jsonMC.respuesta);
+                    } else {
+
+                        self.errorLogin = response.data.jsonMC.error;
+                    }
+
+                    self.parcial.cargando = false;
+                });
+
+        }
+
+        function login (form) {
+
+            console.log(form);
+
+            if (form && form.usuario && form.password) {
+
+                var url = 'https://donweb.com/ajax-login.php';
+                // OAuthProvider: OAuthProvider,
+                // OAuthState: OAuthState
+                $http.jsonp($sce.trustAsResourceUrl(url), {
+                    jsonpCallbackParam: 'jsoncallback',
+                    params: {
+                        account:'old',
+                        usernameLogin: form.usuario,
+                        passLogin: form.password,
+                        accion: 'login'
+                    }
+                })
+                .then(function (response) {
+
+                    if (response && response.jsonMC && response.jsonMC.resultado) {
+
+                        console.log(response.jsonMC.respuesta);
+                    } else {
+
+                        self.errorLogin = response.jsonMC.error;
+                    }
+                });                
             }
 
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
+        }
 
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-
-            divMensaje.innerHTML = data.registrationId;
-        });
-
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
     }
-};
+
+})(_config, window);
